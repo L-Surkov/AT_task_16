cc80592f8b5a9e1a884016de1a926e7e7ef1af23
L-Surkov <ilya.surkov11@gmail.com>
2025-12-10 13:41:12 +0300

    Add config in TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -23,6 +23,9 @@ public class TestBase {
     static void setupEnvironment() {
         Configuration.baseUrl = "https://demoqa.com";
         RestAssured.baseURI = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion ="128.0";
         Configuration.pageLoadStrategy = "eager";
         defaultParser = Parser.JSON;
         Configuration.remote = System.getProperty("remote");
